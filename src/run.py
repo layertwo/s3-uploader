d81d2c86e33e8a056ef7301d959c0ad142f38c3b
Dorian Monnier <dorian.monnier@hifi-filter.com>
2020-08-11 18:49:24 +0200

    ✨ simple Python script
    - Script which watch directory and upload new files to a S3 Bucket

---
+++ src/run.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import logging
+import os
+import time
+import boto3
+from botocore.exceptions import ClientError
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+
+class UploadToS3(FileSystemEventHandler):
+    def __init__(self, bucket_name):
+        self.bucket_name = bucket_name
+        self.client = boto3.client('s3')
+
+    def on_created(self, event):
+        logging.info("Upload %s", event.src_path)
+        try:
+            self.client.upload_file(event.src_path, self.bucket_name, os.path.basename(event.src_path))
+        except ClientError as ex:
+            logging.error(ex)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+    if "S3_BUCKET" not in os.environ or "DIRECTORY" not in os.environ:
+        logging.error("Need two env var: S3_BUCKET and DIRECTORY")
+        exit(1)
+
+    observer = Observer()
+    observer.schedule(UploadToS3(os.environ["S3_BUCKET"]), os.environ["DIRECTORY"], recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
