a13f6cd40675b2f24f4da84eff9912708f1d4946
Lucas Messenger <1335960+layertwo@users.noreply.github.com>
2023-11-14 18:28:24 -0800

    lint: black and isort

--- src/run.py
+++ src/run.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
-
 import logging
 import os
 import time
+
 import boto3
 from botocore.exceptions import ClientError
 from watchdog.events import FileSystemEventHandler
@@ -12,20 +11,24 @@ from watchdog.observers import Observer
 class UploadToS3(FileSystemEventHandler):
     def __init__(self, bucket_name):
         self.bucket_name = bucket_name
-        self.client = boto3.client('s3')
+        self.client = boto3.client("s3")
 
     def on_created(self, event):
         time.sleep(10)  # 10 seconds on Linux, else the input file is 0kb…
         logging.info("%s: uploading…", event.src_path)
         try:
-            self.client.upload_file(event.src_path, self.bucket_name, os.path.basename(event.src_path))
+            self.client.upload_file(
+                event.src_path, self.bucket_name, os.path.basename(event.src_path)
+            )
             logging.info("%s: upload done", event.src_path)
         except Exception as ex:
             logging.error(ex)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+    )
 
     if "S3_BUCKET" not in os.environ or "DIRECTORY" not in os.environ:
         logging.error("Need two env var: S3_BUCKET and DIRECTORY")
